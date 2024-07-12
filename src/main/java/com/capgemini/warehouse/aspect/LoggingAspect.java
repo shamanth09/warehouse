cdc546bcd299523c2ba409dd5170d6efb7de9ce0
shamanth09 <shamanthrev@gmail.com>
2024-07-12 15:32:10 +0200

    docker file update

--- src/main/java/com/capgemini/warehouse/aspect/LoggingAspect.java
+++ src/main/java/com/capgemini/warehouse/aspect/LoggingAspect.java
@@ -18,19 +18,22 @@ public class LoggingAspect {
     @Pointcut("execution(* com.capgemini.warehouse.service..*(..))")
     public void serviceLayer() {}
 
-    @Before("serviceLayer()")
+    @Pointcut("execution(private * com.capgemini.warehouse.service..*(..))")
+    public void privateMethodsInServiceLayer() {}
+
+    @Before("serviceLayer() || privateMethodsInServiceLayer()")
     public void logBefore(JoinPoint joinPoint) {
         logger.info( "Executing method: " + joinPoint.getSignature().getName());
         logger.debug( "Arguments: " + Arrays.toString(joinPoint.getArgs()));
     }
 
-    @AfterReturning(pointcut = "serviceLayer()", returning = "result")
+    @AfterReturning(pointcut = "serviceLayer() || privateMethodsInServiceLayer()", returning = "result")
     public void logAfterReturning(JoinPoint joinPoint, Object result) {
         logger.info( "Method executed successfully: " + joinPoint.getSignature().getName());
         logger.debug( "Result: " + result);
     }
 
-    @AfterThrowing(pointcut = "serviceLayer()", throwing = "error")
+    @AfterThrowing(pointcut = "serviceLayer() || privateMethodsInServiceLayer()", throwing = "error")
     public void logAfterThrowing(JoinPoint joinPoint, Throwable error) {
         logger.error("Exception in method: " + joinPoint.getSignature().getName());
         logger.error("Exception: " + error.getMessage());
